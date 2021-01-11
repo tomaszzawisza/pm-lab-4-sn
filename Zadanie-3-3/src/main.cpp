0f72343b789bbe27427d7553dac303cf785a173b
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2021-01-11 10:26:12 +0100

    Wykonano zadanie 3.3.2 –tekst pływający

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,14 +1,39 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-void setup() {  
-lcd.begin(16, 2);
-lcd.print("Zawisza");
+char tablica[] = "Tomasz";
+int tabLen = 0;
+
+void lcdClear(void);
+
+void setup() {
+    lcd.begin(16, 2);
+    lcd.print("Tomasz Zawisza");
+    tabLen = sizeof(tablica);
+    delay(2000);
 }
 
-void loop() {  
-    lcd.setCursor(0, 1); // po ustawieniu współrzednych wykraczających
-    //poza zakres wyświetlacza tekst/ wyświetlana liczba znika
-    lcd.print(millis() / 1000);
-    delay(500);
+void loop() {
+    for (int i =0; i < (18 - tabLen); i++) {
+        lcdClear();
+        lcd.setCursor(i, 0);
+        lcd.print(tablica);
+        delay(1000);
+    }
+
+
+for (int i = (17 - tabLen); i >= 0; i--){
+  lcdClear();
+  lcd.setCursor(i, 1);
+  lcd.print(tablica);
+  delay(1000);
+}
+}
+
+void lcdClear(void) {
+    lcd.setCursor(0,0);
+    lcd.print("                    ");
+    lcd.setCursor(0,1);
+    lcd.print("                    ");
 }
